ba621c9891bb0f0b0edc2ca39dff81038d0051a5
YiboLiang <yiboliang02@gmail.com>
2023-05-30 15:45:37 -0700

    test

--- test.py
+++ test.py
@@ -1,1 +1,8 @@
-print("hoi")
+import pandas as pd
+import numpy as np
+
+data = pd.read_csv("/Users/yiboliang/Projects/weTheArtist/weTheArtist/spotifyApi/data.csv")
+
+print(data.head())
+
+print(data["url"])
