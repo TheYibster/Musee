18e01b31d04f92abad8298f7d36ea8ec2f11e4a7
Zen <nzy020109@gmail.com>
2024-01-19 00:33:16 -0800

    setup and basic tests

--- spotifyApi/createcsv.py
+++ spotifyApi/createcsv.py
@@ -87,4 +87,4 @@ def main():
 
     print("csv successfully downloaded to path.")
 
-
+main()
--- test.py
+++ test.py
@@ -1,6 +1,27 @@
-import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io import wavfile
+from scipy.fft import fft
 
-data = pd.read_csv("/Users/yiboliang/Projects/weTheArtist/weTheArtist/spotifyApi/data.csv")
+# Load an audio file (WAV format)
+# Replace 'your_audio_file.wav' with the path to your audio file
+fs, data = wavfile.read('wavData//1.wav')
 
-print(data["url"])
+# If the audio file has two channels (stereo), just use one
+if len(data.shape) > 1:
+    data = data[:, 0]
+
+# Apply Fourier Transform using FFT
+n = len(data)
+audio_fft = fft(data)
+frequencies = np.abs(audio_fft[:n // 2])  # Get the absolute value of the real part
+
+# Frequency axis (x-axis) for plotting
+freq_axis = np.linspace(0, fs / 2, int(n / 2))
+
+# Plotting the Frequency Spectrum
+plt.plot(freq_axis, frequencies)
+plt.xlabel('Frequency (Hz)')
+plt.ylabel('Magnitude')
+plt.title('Frequency Spectrum of the Audio')
+plt.show()
