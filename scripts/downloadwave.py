b6049a4bd82809ec78e96e8875481e624d8e7ec6
LenryHouie <henry3.louie@gmail.com>
2024-01-19 18:21:49 -0800

    urlToWav

--- scripts/downloadwave.py
+++ scripts/downloadwave.py
@@ -7,12 +7,11 @@ import os
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('./csvData/cleanUrl.csv')
-for i in df.loc[:,'url']:
-    download_wav(i)
+df = pd.read_csv('../csvData/cleanUrl.csv')
+songs = df['url']
+for i in range(len(songs)):
+    download_wav(songs[i], os.path.join(os.getcwd(), "wavData" + f"{i}.wav"))
 
-
-print(df.head())
 
 
     
